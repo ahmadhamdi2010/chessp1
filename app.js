18ef6aa74e6da7e98961f20e3402a06402e812bb
Ahmad Abdeen <ahmad2013hamdi@gmail.com>
2021-04-05 17:21:57 +0200

    progress

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
-document.getElementById("a8").innerHTML = '♞';
+var gameStart = 0;
 
-document.getElementById("h1").innerHTML = '♘';
+var whiteLocation=[0][7];
+var blackLocation=[7][0];
+
+var board = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+
+function moveWhite(destination) {
+    
+}
+
+function moveBlack(destination) {
+    
+}
+
+
+function displayOptions(currentPsition){
+
+}
+
+function markUsedPosition(position) {
+    
+}
+
+function name(params) {
+    
+}
+
+document.getElementById("07").innerHTML = '♞';
+
+document.getElementById("70").innerHTML = '♘';
+
+document.getElementById("h1").addEventListener("click", displayDate);
+

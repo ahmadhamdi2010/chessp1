e09e8186801ecaba8b85e0f5ed043e10a6f74b03
Ahmad Abdeen <ahmad2013hamdi@gmail.com>
2021-04-05 18:50:01 +0200

    added movement and board update

--- app.js
+++ app.js
@@ -4,22 +4,67 @@ var whiteLocation=[0][7];
 var blackLocation=[7][0];
 
 var board = [
+    [0, 0, 0, 0, 0, 0, 0, 1],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [2, 0, 0, 0, 0, 0, 0, 0],
   ];
 
-function moveWhite(destination) {
-    
+function updateBoard(board) {
+    for(i=0; i<8 ; i++){
+        for(j=0; j<8 ; j++){
+            if(board[i][j]==1){
+                console.log("" +i+j);
+                document.getElementById("" +i+j).innerHTML = '♞';
+            }else if(board[i][j]==2){
+                console.log("" +i+j);
+                document.getElementById("" +i+j).innerHTML = '♘';
+            }else if(board[i][j]==5){
+                document.getElementById("" +i+j).classList.toggle('taken');
+            }else if(board[i][j]==0){
+                document.getElementById("" +i+j).innerHTML = '';
+            }
+        }
+    }   
 }
 
+
 function moveBlack(destination) {
+
+    for(i=0; i<8 ; i++){
+        for(j=0; j<8 ; j++){
+
+            if(board[i][j]==1){
+                board[i][j]=0;
+            }if(''+i+j == destination){
+                board[i][j]=1;
+            }
+        }
+    }
+
+    updateBoard(board);
+
+}
+
+function moveWhite(destination) {
+
+       for(i=0; i<8 ; i++){
+        for(j=0; j<8 ; j++){
+
+            if(board[i][j]==2){
+                board[i][j]=0;
+            }if(''+i+j == destination){
+                board[i][j]=2;
+            }
+        }
+    }
     
+    updateBoard(board);
+
 }
 
 
@@ -35,9 +80,7 @@ function name(params) {
     
 }
 
-document.getElementById("07").innerHTML = '♞';
 
-document.getElementById("70").innerHTML = '♘';
 
-document.getElementById("h1").addEventListener("click", displayDate);
+//document.getElementById("h1").addEventListener("click", displayDate);
 
